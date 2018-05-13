64b41171aa499c8c36f80c9be74417354c9c722b
Rizki Syaputra <rizki@oddbit.id>
2018-05-13 23:46:10 +0800

    change

--- app/src/main/java/superheroku/id/co/lkppapps/newapp/NewApp2Main.java
+++ app/src/main/java/superheroku/id/co/lkppapps/newapp/NewApp2Main.java
@@ -2,16 +2,16 @@ package superheroku.id.co.lkppapps.newapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import superheroku.id.co.lkppapps.R;
-import superheroku.id.co.lkppapps.model.ResultModel;
 
 public class NewApp2Main extends AppCompatActivity {
 
@@ -19,20 +19,14 @@ public class NewApp2Main extends AppCompatActivity {
     String url = "http://www.blueappsoftware.in/";
 
     public ListView listView;
-    @BindView(R.id.dummyTop)
-    TextView dummyTop;
-    @BindView(R.id.bookName)
-    TextView bookName;
-    @BindView(R.id.bookAuthor)
-    TextView bookAuthor;
-    @BindView(R.id.dummyBottom)
-    TextView dummyBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.data_layout);
-        ButterKnife.bind(this);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.bookList);
+
+
 
 
         loadSize();
@@ -40,27 +34,25 @@ public class NewApp2Main extends AppCompatActivity {
 
     private void loadSize() {
         service serviceAPI = RestClient.getClient();
-        Call<ResultModel> loadData = serviceAPI.loadSizes();
-        loadData.enqueue(new Callback<ResultModel>() {
+        Call<List<ResponNewLkppResponse>> loadSizeCall = serviceAPI.loadSizes();
+        loadSizeCall.enqueue(new Callback<List<ResponNewLkppResponse>>() {
             @Override
-            public void onResponse(Call<ResultModel> call, Response<ResultModel> response) {
-                response.body(); // have your all data
-                int id = response.body().getIdSatker();
-                String userName = response.body().getNamaKpa();
-                String level = response.body().getAkhirPengadaan();
+            public void onResponse(Call<List<ResponNewLkppResponse>> call, Response<List<ResponNewLkppResponse>> response) {
+                Log.d("data",  "Kode :" + response.body());
+//                Log.d("Tag","Cek Data :"+new Gson().toJson(data));
 
-                dummyBottom.setText(userName);
-                dummyTop.setText(level);
-
-
+                List<ResponNewLkppResponse> data = response.body();
+//                Toast.makeText(getApplicationContext(), "Body", Toast.LENGTH_LONG).show();
+//                // Set response Books as listed layout
+//                DataAdapter booksAdapter = new DataAdapter(getBaseContext(), R.layout.data_layout, (ArrayList<NewLkppResponse>) data);
+//                listView.setAdapter(booksAdapter);
             }
 
             @Override
-            public void onFailure(Call<ResultModel> call, Throwable t) {
-
+            public void onFailure(Call<List<ResponNewLkppResponse>> call, Throwable t) {
+                System.out.println(t.getMessage());
             }
         });
-
     }
 
 
